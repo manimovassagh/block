5035c1ac32dd1af8afb1e1703410e6b6d3e4221e
Mani Movassagh <mani.mgh@gmail.com>
2025-02-13 21:40:26 +0100

    fix: enhance logging in blockchain process and add PrintBlockchain function

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
 	"time"
 
 	"github.com/manimovassagh/go-clock/models"
@@ -25,8 +28,11 @@ func (bc *Blockchain) GetLatestBlock() models.Block {
 
 func (bc *Blockchain) AddBlock(newBlock models.Block) {
 	newBlock.PreviousHash = bc.GetLatestBlock().Hash
+	log.Printf("Adding new block with index %d and previous hash %s", newBlock.Index, newBlock.PreviousHash)
 	newBlock.MineBlock(bc.Difficulty) // Use stable MineBlock function
 	bc.Chain = append(bc.Chain, newBlock)
+	log.Printf("Block added with hash %s", newBlock.Hash)
+	bc.PrintBlockchain()
 }
 
 func (bc *Blockchain) IsChainValid() bool {
@@ -45,6 +51,16 @@ func (bc *Blockchain) IsChainValid() bool {
 	return true
 }
 
+func (bc *Blockchain) PrintBlockchain() {
+	blockchainJSON, err := json.MarshalIndent(bc, "", "  ")
+	if err != nil {
+		log.Println("Error marshalling blockchain:", err)
+		return
+	}
+	fmt.Println(string(blockchainJSON))
+	log.Println(string(blockchainJSON))
+}
+
 func NewBlockchain() *Blockchain {
 	return &Blockchain{
 		Chain:      []models.Block{createGenesisBlock()},
@@ -53,6 +69,15 @@ func NewBlockchain() *Blockchain {
 }
 
 func main() {
+	// Set up logging to a file
+	logFile, err := os.OpenFile("blockchain.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("Failed to open log file:", err)
+		return
+	}
+	defer logFile.Close()
+	log.SetOutput(logFile)
+
 	block := models.Block{
 		Index:        1,
 		Timestamp:    time.Now().String(),
@@ -62,6 +87,12 @@ func main() {
 
 	difficulty := 5 // Increase the difficulty level
 
-	fmt.Println("Mining block...")
+	log.Println("Starting mining process...")
 	block.MineBlock(difficulty) // Use stable MineBlock function
+	log.Println("Mining process completed")
+
+	blockchain := NewBlockchain()
+	blockchain.AddBlock(block)
+	log.Println("Blockchain is valid:", blockchain.IsChainValid())
+	blockchain.PrintBlockchain()
 }
